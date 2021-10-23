99e900c956ecb3a37fd39e4534b6c2b9bbf87ae9
AleX-PirS <78353806+AleX-PirS@users.noreply.github.com>
2021-10-23 13:42:50 +0300

    Update unit_test.py

--- homework_1/unit_test.py
+++ homework_1/unit_test.py
@@ -7,22 +7,86 @@ class TestGame(unittest.TestCase):
         self.game = tictactoe_class.TicTacToe()
 
     def test_win_checker(self):
+
         board = ['x', '0', '0', 'x', 'x', 'x', '0', '0', ' ']
         self.assertEqual(tictactoe_class.TicTacToe().win_checker(board), 1)
+
+        board = ['x', '0', '0', '0', 'x', '0', 'x', 'x', 'x']
+        self.assertEqual(tictactoe_class.TicTacToe().win_checker(board), 1)
+
+        board = ['x', ' ', '0', ' ', 'x', ' ', ' ', '0', 'x']
+        self.assertEqual(tictactoe_class.TicTacToe().win_checker(board), 1)
+
+        board = ['x', 'x', '0', ' ', '0', ' ', '0', ' ', ' ']
+        self.assertEqual(tictactoe_class.TicTacToe().win_checker(board), 1)
+
+        board = ['x', '0', '0', 'x', ' ', ' ', 'x', ' ', ' ']
+        self.assertEqual(tictactoe_class.TicTacToe().win_checker(board), 1)
+
+        board = ['0', '0', 'x', ' ', ' ', 'x', ' ', ' ', 'x']
+        self.assertEqual(tictactoe_class.TicTacToe().win_checker(board), 1)
+
         board = ['0', '0', 'x', 'x', 'x', '0', '0', 'x', '0']
         self.assertEqual(tictactoe_class.TicTacToe().win_checker(board), 1000)
+
         board = ['0', ' ', ' ', 'x', ' ', ' ', ' ', ' ', ' ']
         self.assertEqual(tictactoe_class.TicTacToe().win_checker(board), None)
 
+    def test_input_checker(self):
+
+        board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = '1'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board)[0], 'done')
+
+        board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = '9'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board)[0], 'done')
+
+        board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = '10'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board), 'Out of range!')
+
+        board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = '0'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board), 'Out of range!')
+
+        board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = '-10'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board), 'Only digit input!')
+
+        board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = '4а'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board), 'Only digit input!')
+
+        board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = 'zero'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board), 'Only digit input!')
+
+        board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = 'a'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board), 'Only digit input!')
+
+        board = ['0', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        quad = '1'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board), 'This place is already pinned!')
+
+        board = [' ', ' ', ' ', ' ', '0', ' ', ' ', ' ', ' ']
+        quad = '5'
+        self.assertEqual(tictactoe_class.TicTacToe().input_checker(quad, board), 'This place is already pinned!')
+
     def test_rename(self):
+
         player = tictactoe_class.Player(test=1)
         self.assertEqual(tictactoe_class.Player(test=1).name, 'test')
+
         player.rename('Alice')
         self.assertEqual(player.name, 'Alice')
 
     def test_set_score(self):
+
         player = tictactoe_class.Player(test=1)
         self.assertEqual(player.score, 0)
+
         player.set_score(50)
         self.assertEqual(player.score, 50)
 
